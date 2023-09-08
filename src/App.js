084f0148814ce7e2a70565f40842cbe7d26bfe10
Nick Chernock <n.chernock@gmail.com>
2023-09-08 00:47:11 -0400

    true initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react"
 
-function App() {
+
+
+function App({cookList}) {
+
+use 
+let [money, setMoney] = useState(1);
+let [clcOwned, setClcOwned] = useState(0);
+
+let idleBuy = function() {
+  setInterval(
+    function() {
+      setMoney((money) => money+(clcOwned));
+  }, 999);
+}
+
+let clcBuy = function() {
+    setClcOwned(++clcOwned);
+
+  idleBuy();
+}
+
+let cookPress = function() {
+  setMoney(money+1);
+  console.log(money);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Crafting Menu */}
+      <div className='craft'>
+        <div>
+          <h4>Crummy Line Cook</h4>
+          <h5>Owned: {clcOwned}</h5>
+          <button onClick={clcBuy}>Buy 1</button>
+        </div>
+      </div>
+      {/* Click Area */}
+      <div className='clickArea'>
+          <span>{money}</span>
+          <button onClick={cookPress}>Cook</button>
+      </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/cookList.js
@@ -0,0 +1,22 @@
+let cookList = [
+    {
+        name: "Crumby Line Cook",
+        cost: 50,
+        increase: 1
+    },
+    {
+        name: "Strung Out Cook",
+        cost: 250,
+        increase: 5
+    },
+    {
+        name: "Junkie Cook",
+        cost: 1000,
+        increase: 25
+    },
+    {
+        name: "High School Cook",
+        cost: 10000,
+        increase: 100
+    },
+];
