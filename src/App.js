a04a1a3934ae65db33f87578edbb0e741726ffeb
Nick Chernock <n.chernock@gmail.com>
2023-09-12 02:14:07 -0400

    data list + incremental increases
    - finished a simple component to display a fixed list of cooks
    
    - started the incremental increase functions (WIP)
    
    - minor styling

--- src/App.js
+++ src/App.js
@@ -1,47 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React, { useEffect, useState } from "react"
-import cookList from './cookList'
+import LineCooks from './lineCooks';
+import './lineCooks.css'
 
 
-
-function App({cookList}) {
-
- 
+function App() {
+  
 let [money, setMoney] = useState(1);
-let [clcOwned, setClcOwned] = useState(0);
+let [intervalId, setIntervalId] = useState(null);
 
-let idleBuy = function() {
-  setInterval(
-    function() {
-      setMoney((money) => money+(clcOwned));
-  }, 999);
-}
-
-let clcBuy = function() {
-    setClcOwned(++clcOwned);
-
-  idleBuy();
-}
+useEffect(() => {
+  if(intervalId) {
+    return () => clearInterval(intervalId);
+  }
+}, [intervalId])
 
 let cookPress = function() {
   setMoney(money+1);
   console.log(money);
 }
 
+function buyPress(increaseAmt) {
+  if(!intervalId) {
+    let id = setInterval(() => {
+      setMoney((money) => money + increaseAmt);
+    }, 1000);
+    setIntervalId(id);
+  }
+}
 
-
+function clearPress() {
+  setMoney(0);
+  clearInterval(intervalId);
+  setIntervalId(null);
+}
 
   return (
     <div className="App">
       {/* Crafting Menu */}
       <div className='craft'>
-        <lineCooks/>
+        <LineCooks buyPress={buyPress} />
       </div>
       {/* Click Area */}
       <div className='clickArea'>
           <span>{money}</span>
           <button onClick={cookPress}>Cook</button>
+          <button onClick={clearPress}>Clear</button>
       </div>
     </div>
   );
--- src/lineCooks.js
+++ src/lineCooks.js
@@ -1,29 +1,48 @@
 import React from "react";
-import cookList from './cookList'
 
-class lineCooks extends React.Component {
 
-    constructor() {
-        super();
-        this.state = {
-          // initial state
-        };
-      }
 
-    render() {
 
-    let cooks = cookList.map((cook) =>
+function LineCooks({ buyPress }) {
+
+    {/* Set cook list */}
+    let cookList = [
+        {
+            name: "Crumby Line Cook",
+            cost: 50,
+            increase: 1
+        },
+        {
+            name: "Strung Out Cook",
+            cost: 250,
+            increase: 5
+        },
+        {
+            name: "Junkie Cook",
+            cost: 1000,
+            increase: 25
+        },
+        {
+            name: "High School Cook",
+            cost: 10000,
+            increase: 100
+        },
+    ];
+    
+
+    
+    
+    return (
+        cookList.map((cook) => 
         <div className='cook1'>
             <span key={cook.name}>{cook.name}</span> 
-            <span key={cook.cost}>{cook.cost}</span>
-            <span key={cook.increase}>{cook.increase}</span>
+            <span key={cook.cost}>Cost: {cook.cost}</span>
+            <span key={cook.increase}>{cook.increase} Points /s </span>
+            <button onClick={() => buyPress(cook.increase)}>Buy</button>
         </div>
-    );
-        
-    return (
-        {cooks}
+        )
     );
 
+}
 
-}
-}
+export default LineCooks;
